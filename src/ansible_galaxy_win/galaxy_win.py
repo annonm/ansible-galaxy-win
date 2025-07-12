cd61761ce41a00f4a1fcea4f3d377f867e807cbb
annon <annonm@gmail.com>
2025-07-12 12:58:22 -0400

    Cleanup

--- src/ansible_galaxy_win/galaxy_win.py
+++ src/ansible_galaxy_win/galaxy_win.py
@@ -5,7 +5,7 @@ locale.getlocale = lambda: ('en_US', 'UTF-8')
 
 dummy_termios = types.ModuleType("termios")
 
-# Constants (values are placeholders)
+# Values don't matter here
 dummy_termios.IFLAG = 0
 dummy_termios.OFLAG = 1
 dummy_termios.CFLAG = 2
@@ -13,7 +13,6 @@ dummy_termios.LFLAG = 3
 dummy_termios.ISPEED = 4
 dummy_termios.OSPEED = 5
 dummy_termios.CC = 6
-
 dummy_termios.VINTR = 0
 dummy_termios.VQUIT = 1
 dummy_termios.VERASE = 2
@@ -31,7 +30,6 @@ dummy_termios.VDISCARD = 13
 dummy_termios.VWERASE = 14
 dummy_termios.VLNEXT = 15
 dummy_termios.VEOL2 = 16
-
 dummy_termios.B0 = 0
 dummy_termios.B50 = 50
 dummy_termios.B75 = 75
@@ -48,7 +46,6 @@ dummy_termios.B4800 = 4800
 dummy_termios.B9600 = 9600
 dummy_termios.B19200 = 19200
 dummy_termios.B38400 = 38400
-
 dummy_termios.TCSANOW = 0
 dummy_termios.TCSADRAIN = 1
 dummy_termios.TCSAFLUSH = 2
@@ -75,7 +72,7 @@ def tcflow(fd, action):
 def tcsendbreak(fd, duration):
     pass
 
-# Assign functions
+# Functions, in case you need more control
 dummy_termios.tcgetattr = tcgetattr
 dummy_termios.tcsetattr = tcsetattr
 dummy_termios.tcflush = tcflush
@@ -83,18 +80,20 @@ dummy_termios.tcdrain = tcdrain
 dummy_termios.tcflow = tcflow
 dummy_termios.tcsendbreak = tcsendbreak
 
-sys.modules["termios"] =dummy_termios
+sys.modules["termios"] = dummy_termios
 
 pointless_imports = ["fcntl", "fork"]
 
 for dummy in ["fcntl", "fork", "grp", "pwd"]:
     sys.modules[dummy] = types.ModuleType(dummy)
 
+# Mitigate forking issues
 import multiprocessing
 fake_context = types.SimpleNamespace()
 fake_context.parent_process = lambda: "FAKE"
 multiprocessing.get_context = lambda method=None: fake_context
 
+# 😭😭😭
 import ctypes
 import ctypes.util
 
@@ -104,23 +103,35 @@ original_load_library = ctypes.cdll.LoadLibrary
 # Define a fake libc object
 class FakeLibC:
     def __getattr__(self, name):
+        # Useful info, incase something goes wrong
         print(f"[FAKE_LIBC] Called function: {name}")
         return lambda *args, **kwargs: 0  # dummy function that returns 0
 
 # Patch it
 ctypes.cdll.LoadLibrary = lambda libname: FakeLibC() if libname == ctypes.util.find_library('c') else original_load_library(libname)
 
+# Ansible whyyyyyy did you use an OS separator in a compiled regex /ansible/parsing/dataloader.py#30
 import os
 os.path.sep = '/'
 
-from ansible.cli.galaxy import main as galaxy_main
 
-args = [
-    '',
-    'collection',
-    'download',
-    'community.general'
-]
+def execute_download_win(galaxy_commandline_args):
+    """
+    Convenience wrapper for downloading some collection.
 
-g = galaxy_main(args=args)
-g.execute_download()
+    If you don't want to call the Ansible Galaxy APIs in your own code, just call this function. Otherwise, import this
+    module BEFORE you import ANY Ansible modules in your own code (very important!), and then call execute_download()
+    yourself.
+
+    :param galaxy_commandline_args: List[str] representing the arguments you would pass when calling ansible-galaxy on
+    the command line. Example:
+    To represent calling "ansible-galaxy collection download community.general"
+    Call with something like:
+     args = ['collection', 'download', 'community.general']
+     execute_download_win(args)
+    """
+    # First element is not the first argument
+    args = [None] + galaxy_commandline_args
+    from ansible.cli.galaxy import main as galaxy_main
+    galaxy = galaxy_main(args=args)
+    galaxy.execute_download()
